bb6e3a9a7d41d51c55b30442b1cf767730be002f
Matthew <matthewjwright07@gmail.com>
2026-01-07 18:46:40 -0600

    more updates

--- lander/components/pages/HomePage.tsx
+++ lander/components/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import Header from "../layout/Header";
-import Hero from "../Elements/hero/Hero";
+import Hero from "../elements/hero/Hero";
 import Features from "../elements/features/Features"
 import Pricing from "../elements/pricing/Pricing"
 import Walkthrough from "../elements/walkthrough/Walkthrough"
