9f6baedab010fb86edc3fd1e8afdecb2b77aba51
Matthew <matthewjwright07@gmail.com>
2026-01-07 20:36:02 -0600

    service account key

--- api/utils/firebaseServer.js
+++ api/utils/firebaseServer.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 
+const serviceAccountKey = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, "base64").toString("utf8")
+);
+
 admin.initializeApp({
-  credential: admin.credential.cert(
-    require("../serviceAccountKey.json")
-  ),
+  credential: admin.credential.cert(serviceAccountKey),
 });
 
 module.exports = admin;
