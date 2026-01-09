d39eb87dc4228eea16e1b57fdef6de910b912b6c
Matthew <matthewjwright07@gmail.com>
2026-01-09 00:33:28 -0600

    contact page

--- lander/components/elements/contact/Contact.tsx
+++ lander/components/elements/contact/Contact.tsx
@@ -7,8 +7,8 @@ export default function Contact(){
             <PaddingY>
                 <div className="flex flex-col gap-8 w-full">
                     <h2 className="text-4xl font-bold text-white">Contact</h2>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                        <img src="https://wallpapers.com/images/hd/glowing-contact-us-logo-qx58564q7a51etk6.jpg" className="w-full rounded-lg"/>
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 border border-white p-16 rounded-lg shadow-[0_0_20px_0] shadow-primary">
+                        <img src="/contact.svg" className="h-full rounded-lg"/>
                         <div className="flex flex-col gap-4">
                             <h3 className="text-white text-2xl font-bold">Let's Get In Touch</h3>
                             <input type="text" className="border border-white p-4 rounded-lg text-white placeholder-secondary" placeholder="Name"/>
