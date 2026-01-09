1346968e9db29cdc1b37336b9f760f83d3ed98cd
Matthew <matthewjwright07@gmail.com>
2026-01-09 00:37:11 -0600

    contact page updates

--- lander/components/elements/contact/Contact.tsx
+++ lander/components/elements/contact/Contact.tsx
@@ -7,8 +7,17 @@ export default function Contact(){
             <PaddingY>
                 <div className="flex flex-col gap-8 w-full">
                     <h2 className="text-4xl font-bold text-white">Contact</h2>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 border border-white p-16 rounded-lg shadow-[0_0_20px_0] shadow-primary">
-                        <img src="/contact.svg" className="h-full rounded-lg"/>
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 rounded-lg py-8">
+                        <div className="flex flex-col gap-8">
+                            <div className="gap-1">
+                                <h3 className="text-white text-2xl font-bold">Email:</h3>
+                                <h3 className="text-white text-2xl font-bold">support@elementy.io</h3>
+                            </div>
+                            <div className="gap-1">
+                                <h3 className="text-white text-2xl font-bold">Phone:</h3>
+                                <h3 className="text-white text-2xl font-bold">+1 (512) 586-4786</h3>
+                            </div>
+                        </div>
                         <div className="flex flex-col gap-4">
                             <h3 className="text-white text-2xl font-bold">Let's Get In Touch</h3>
                             <input type="text" className="border border-white p-4 rounded-lg text-white placeholder-secondary" placeholder="Name"/>
